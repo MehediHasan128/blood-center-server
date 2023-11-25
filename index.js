e7ab4d790249acb640252230766c80841035778c
MehediHasan128 <mehedihasan12926@gmail.com>
2023-11-25 16:59:22 +0530

    create jwt post api and set token verify middelware

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const jwt = require('jasonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jmtxbp5.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,48 +25,64 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const dictrictColletion = client.db('bloodCenterDB').collection('districts');
-    const upazilaCollection = client.db('bloodCenterDB').collection('upazila');
-    const userCollection = client.db('bloodCenterDB').collection('users');
+    const dictrictColletion = client.db("bloodCenterDB").collection("districts");
+    const upazilaCollection = client.db("bloodCenterDB").collection("upazila");
+    const userCollection = client.db("bloodCenterDB").collection("users");
+
+
+    // Custom Middelware
+    const verifyToken = (req, res, next) =>{
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorized access'})
+      }
+
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.SECREAT_TOKEN, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
 
 
     // Json web token relateld api
-    app.post('/jwt', async(req, res) =>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      
-    })
-
-
-
+      const token = jwt.sign(user, process.env.SECREAT_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     // District related api
-    app.get('/districts', async(req, res) =>{
-        const result = await dictrictColletion.find().toArray();
-        res.send(result);
-    })
+    app.get("/districts", async (req, res) => {
+      const result = await dictrictColletion.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/upazila/:name', async(req, res) =>{
-        const districtName = req.params.name;
-        const query = {district_name: districtName}
-        const result = await upazilaCollection.find(query).toArray();
-        res.send(result);
-    })
-
-
+    app.get("/upazila/:name", async (req, res) => {
+      const districtName = req.params.name;
+      const query = { district_name: districtName };
+      const result = await upazilaCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // User related api
-    
-    app.post('/users', async(req, res) =>{
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const result = await userCollection.insertOne(user)
+      const result = await userCollection.insertOne(user);
       res.send(result);
-    })
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -78,17 +90,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("The blood center server is running");
+});
 
-
-
-
-
-
-
-app.get('/', (req, res) =>{
-    res.send('The blood center server is running');
-})
-
-app.listen(port, () =>{
-    console.log(`This ser is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`This ser is running on port ${port}`);
+});
