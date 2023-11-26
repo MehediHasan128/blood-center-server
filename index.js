d26bba14c62624c1672ccef98820478a3d56913a
MehediHasan128 <mehedihasan12926@gmail.com>
2023-11-26 15:35:58 +0530

    create patch method to update donation status

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -80,14 +80,35 @@ async function run() {
 
     // Donatin request realted api
 
+    app.get('/allDonationRequest', async(req, res) =>{
+      const result = await donationRequestCollection.find().toArray();
+      res.send(result)
+    })
+
     app.get('/donationRequest', async(req, res) =>{
-      
+      const email = req.query.email;
+      const query = { requesterEmail: email };
+      const result = await donationRequestCollection.find(query).toArray();
+      res.send(result);
     })
 
     app.post('/donationRequest', async(req, res) =>{
         const request = req.body;
         const result = await donationRequestCollection.insertOne(request);
         res.send(result);
+    })
+
+    app.patch('/donationRequest/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const action = req.body;
+      const updateDoc = {
+        $set: {
+          Status: action.Status
+        },
+      };
+      const result = await donationRequestCollection.updateOne(filter, updateDoc);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
